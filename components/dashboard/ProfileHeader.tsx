2cec9057826997a3bf686e25fbcc2f8582b75475
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-03-22 10:57:08 +0100

    Update ProfileHeader.tsx

--- components/dashboard/ProfileHeader.tsx
+++ components/dashboard/ProfileHeader.tsx
@@ -13,10 +13,12 @@ import {
 
 const ProfileHeader: React.FC = () => {
   return (
-    <div className="relative w-full h-[250px] newsletter-bg rounded-[40px] bg-gradient-to-br from-[#4f3130] to-[#323232] p-6 text-white shadow-lg flex justify-center flex-col gap-10 pt-16">
-      <div className="flex w-full gap-28">
-        <div className="flex gap-5">
-          <div className="relative w-20 h-20 mx-auto">
+    <div className="relative w-full h-[250px] newsletter-bg rounded-[40px] bg-gradient-to-br from-[#4f3130] to-[#323232] p-6 text-white shadow-lg flex flex-col gap-6 pt-16">
+      {/* Top Section */}
+      <div className="flex flex-col sm:flex-row w-full gap-4 sm:gap-28 items-center">
+        {/* Profile Info */}
+        <div className="flex items-center gap-5">
+          <div className="relative w-20 h-20 mx-auto sm:mx-0">
             <Image
               src="/jax.png"
               alt="Jaxson Siphron"
@@ -28,14 +30,15 @@ const ProfileHeader: React.FC = () => {
               <FaPen size={12} />
             </div>
           </div>
-          <div className="mt-4 flex flex-col">
+          <div className="flex flex-col">
             <h2 className="text-xl font-semibold">Jaxson Siphron</h2>
             <p className="text-gray-300 text-sm">
               Jaxsonsiphron@gmail.com
             </p>
           </div>
         </div>
-        <div className="mt-6 flex justify-center gap-4">
+        {/* Social Links */}
+        <div className="flex justify-center gap-4">
           <Link href="#" className="hover:text-gray-400">
             <FaFacebook size={20} />
           </Link>
@@ -52,12 +55,12 @@ const ProfileHeader: React.FC = () => {
             <FaYoutube size={20} />
           </Link>
         </div>
-        <div className="absolute border left-32 w-[72%] top-32 border-[#AAAAAA]"></div>
+        <div className="hidden sm:block absolute border left-32 w-[72%] top-32 border-[#AAAAAA]"></div>
       </div>
+      {/* Bottom Section */}
       <div className="flex gap-14 justify-center w-full items-center text-gray-300 uppercase text-xs">
         <div className="flex flex-col gap-2">
           <h3 className="block text-gray-400">Age</h3>
-          {/* For mobile, age numbers display horizontally; on desktop, they stack vertically */}
           <div className="flex flex-row sm:flex-col gap-1">
             <p className="text-2xl font-bold text-white">2</p>
             <p className="text-2xl font-bold text-white">8</p>
