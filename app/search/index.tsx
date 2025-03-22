9c0756ea076735d1df7589f0e808b927a2391ee8
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-03-22 10:13:00 +0100

    Update index.tsx

--- app/search/index.tsx
+++ app/search/index.tsx
@@ -21,15 +21,15 @@ export default function SearchPage() {
       <SearchHeader />
 
       <main className="container mx-auto px-6 md:px-12 py-10 flex-grow">
-        <div className="mb-8 flex flex-col md:flex-row items-start gap-40">
-        <div className="flex-1">
-  <h1 className="text-5xl md:text-7xl font-bold">SEARCH...</h1>
-  <p className="text-base md:text-lg mt-2">
-    Stay Ahead of the Curve with Quiktis’ Cutting-Edge Solutions
-  </p>
-</div>
-
-          <div className="ml-auto">
+        {/* Top Section */}
+        <div className="mb-8 flex flex-col md:flex-row items-start gap-8 md:gap-40">
+          <div className="flex-1">
+            <h1 className="text-5xl md:text-7xl font-bold">SEARCH...</h1>
+            <p className="text-base md:text-lg mt-2">
+              Stay Ahead of the Curve with Quiktis’ Cutting-Edge Solutions
+            </p>
+          </div>
+          <div className="mt-8 md:mt-0 md:ml-auto">
             <TagFilters />
           </div>
         </div>
