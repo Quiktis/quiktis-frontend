9a0ccb0e454d2c846ab1ebc1192892947b86bf3b
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-03-22 13:15:48 +0100

    Update index.tsx

--- app/history/index.tsx
+++ app/history/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState } from "react";
 import UserInfoBoxes from "@/components/dashboard/UserInfoBoxes";
 import SearchBar from "@/components/ui/SearchBar";
@@ -22,11 +21,11 @@ const HistoryPage = () => {
   ];
 
   return (
-    <main className="p-8 bg-transparent min-h-screen text-white">
-      <div className="flex flex-col md:flex-row gap-8 items-start mb-8 mt-10">
-        {" "}
+    <main className="p-4 md:p-8 bg-transparent min-h-screen text-white">
+      {/* Top Section */}
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8 items-start mb-8 mt-10">
         {/* Left Column */}
-        <div className="md:w-[400px]">
+        <div className="w-full md:w-[400px]">
           <UserInfoBoxes
             name="Maren Botosh"
             email="maren@example.com"
@@ -34,8 +33,8 @@ const HistoryPage = () => {
             address="2715 Ash Dr. San Jose, South Dakota 83475"
           />
         </div>
-        {/* Right Column*/}
-        <div className="flex-1 flex justify-end">
+        {/* Right Column */}
+        <div className="w-full md:flex-1 flex justify-center md:justify-end">
           <SearchBar
             placeholder="Search any event"
             value={searchValue}
@@ -46,8 +45,7 @@ const HistoryPage = () => {
       </div>
 
       {/* Recent History Table Section */}
-      <div className="mt-20">
-        {" "}
+      <div className="mt-10 md:mt-20">
         <RecentHistoryTable
           data={historyData}
           startDate="04/05/2024"
