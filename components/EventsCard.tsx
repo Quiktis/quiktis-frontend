bfe13a136fe21a78f2b492aa5a86a242f1b317aa
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-03-26 22:26:16 +0100

    Update EventsCard.tsx

--- components/EventsCard.tsx
+++ components/EventsCard.tsx
@@ -1,14 +1,14 @@
-"use client"
-import Image from 'next/image';
-import React from 'react';
-import Button from './ui/Button';
-import { IoTicketSharp } from 'react-icons/io5';
-import { FaLongArrowAltRight } from 'react-icons/fa';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+"use client";
+import Image from "next/image";
+import React from "react";
+import Button from "./ui/Button";
+import { IoTicketSharp } from "react-icons/io5";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface EventProps {
-  eventId: string; //
+  eventId: string;
   image: string;
   alt: string;
   price: string;
@@ -16,7 +16,7 @@ interface EventProps {
   heading: string;
   title: string;
   description: string;
-  date: string; // Changed to string for better formatting
+  date: string;
   country: string;
   slug: string;
 }
@@ -34,34 +34,40 @@ const EventsCard: React.FC<EventProps> = ({
   country,
   slug,
 }) => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <div className='w-[300px] h-[615px] cursor-pointer flex flex-col gap-4' onClick={()=>router.push(`/events${slug}`)}>
-      <div className='relative'>
+    <div
+      className="w-[300px] h-[615px] cursor-pointer flex flex-col gap-4"
+      onClick={() => router.push(`/events${slug}`)}>
+      <div className="relative">
         <Image
           src={image}
           alt={alt}
           width={300}
           height={320}
-          className='rounded-[15px] object-cover bg-cover h-[320px] w-[300px]'
+          className="rounded-[15px] object-cover bg-cover h-[320px] w-[300px]"
         />
-        <div className='absolute h-full flex flex-col justify-between right-4 bottom-0 py-5'>
-          <div className='px-2 rounded-[25px] py-2 flex justify-center items-center newsletter-bg'>
+        <div className="absolute h-full flex flex-col justify-between right-4 bottom-0 py-5">
+          <div className="px-2 rounded-[25px] py-2 flex justify-center items-center newsletter-bg">
             {price}
           </div>
-          <Button className='bg-primary flex gap-3 justify-center text-white py-3 items-center rounded-[15px]' onClick={onClick}>
+          <Button
+            className="bg-primary flex gap-3 justify-center text-white py-3 items-center rounded-[15px]"
+            onClick={onClick}>
             Get Ticket
             <IoTicketSharp />
           </Button>
         </div>
       </div>
-      <h2 className='text-[16px]'>{heading}</h2>
-      <h4 className='text-[#666666] text-[12px]'>{title}</h4>
-      <p className='text-[12px]'>{description}</p>
-      <div className='flex justify-between items-center'>
-        <div className='w-[5em] text-sm'>{date}</div>
-        <h4 className='mx-auto text-sm  text-gray-500'>{country}</h4>
-        <Link href={slug} className='text-[0.75em] flex gap-2 justify-center items-center border rounded-md px-3 py-[0.3em]'>
+      <h2 className="text-[16px]">{heading}</h2>
+      <h4 className="text-[#666666] text-[12px]">{title}</h4>
+      <p className="text-[12px]">{description}</p>
+      <div className="flex justify-between items-center">
+        <div className="w-[5em] text-sm">{date}</div>
+        <h4 className="mx-auto text-sm  text-gray-500">{country}</h4>
+        <Link
+          href={slug}
+          className="text-[0.75em] flex gap-2 justify-center items-center border rounded-md px-3 py-[0.3em]">
           Read more
           <FaLongArrowAltRight />
         </Link>
