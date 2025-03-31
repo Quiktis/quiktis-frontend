46103f490ed9a468f89e25eea032f97ec66dd1de
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-03-31 16:16:26 +0100

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,32 +1,35 @@
 export const strongPoints = [
-    {
-      title: "Security Built on the Blockchain",
-      description: "Blockchain-powered security for tamper-proof transactions and total transparency—no middlemen, no compromises.",
-      iconUrl: "/strong_point1.png"
-    },
-    {
-      title: "Decentralized Identity",
-      description: "No third parties, no data breaches—just you, in control.",
-      iconUrl: "/strong_point2.png"
-    },
-    {
-      title: "Smart Contract Automation",
-      description: "Streamline operations with automated smart contracts.",
-      iconUrl: "/strong_point3.png"
-    },
-    {
-      title: "Community Governance",
-      description: "Participate in decentralized decision-making through voting.",
-      iconUrl: "/strong_point6.png"
-    },
-    {
-      title: "Asset Tokenization",
-      description: "Digitally tokenize real-world assets for efficient management.",
-      iconUrl: "/strong_point5.png"
-    },
-    {
-      title: "Get Refund on Unsed Ticket",
-      description: "Unused ticket? No worries. Easily request a refund—fast, secure, and hassle-free.",
-      iconUrl: "/strong_point6.png"
-    }
-  ]
+  {
+    title: "Security Built on the Blockchain",
+    description:
+      "Blockchain-powered security for tamper-proof transactions and total transparency—no middlemen, no compromises.",
+    iconUrl: "/strong_point1.png",
+  },
+  {
+    title: "Decentralized Identity",
+    description: "No third parties, no data breaches—just you, in control.",
+    iconUrl: "/strong_point2.png",
+  },
+  {
+    title: "Smart Contract Automation",
+    description: "Streamline operations with automated smart contracts.",
+    iconUrl: "/strong_point3.png",
+  },
+  {
+    title: "Community Governance",
+    description: "Participate in decentralized decision-making through voting.",
+    iconUrl: "/strong_point6.png",
+  },
+  {
+    title: "Asset Tokenization",
+    description:
+      "Digitally tokenize real-world assets for efficient management.",
+    iconUrl: "/strong_point5.png",
+  },
+  {
+    title: "Get Refund on Unsed Ticket",
+    description:
+      "Unused ticket? No worries. Easily request a refund—fast, secure, and hassle-free.",
+    iconUrl: "/strong_point6.png",
+  },
+];
