e7ecc102211f139d5a5290bb42bcb985ec1c6708
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-04-05 12:56:04 +0100

    Update page.tsx

--- app/new-landing-page/page.tsx
+++ app/new-landing-page/page.tsx
@@ -188,7 +188,6 @@ export default function NewLandingPage() {
         <TrustedBrands containerClass="my-2" />
       </section>
 
-      {/* Strong Points Section */}
       <section className="max-sm:px-0 px-3 lg:px-[5em] gap-5 py-[6em] md:py-[10em]">
         <div className="flex flex-col justify-center md:grid md:grid-cols-2 lg:grid-cols-3 gap-[2.5em] w-fit mx-auto max-sm:w-[95%]">
           {strongPoints.map((item, index) => (
@@ -202,7 +201,7 @@ export default function NewLandingPage() {
                 width={80}
                 height={80}
               />
-              <p className={`${index === strongPoints.length - 1 ? "" : "text-[#D3D3D6]"}`}>
+              <p className={`${index === strongPoints.length - 1 ? "" : "text-[#1e1e1e]"}`}>
                 {item.description}
               </p>
             </div>
