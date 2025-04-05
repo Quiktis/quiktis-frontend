13ec167c7e9b3c3d9ae9fd6ce0402f3d5e41ce00
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-04-05 16:49:13 +0100

    Update EventStats.tsx

--- components/dashboard/EventStats.tsx
+++ components/dashboard/EventStats.tsx
@@ -47,8 +47,8 @@ const EventStats: React.FC = () => {
         ))}
       </div>
 
-      <div className="absolute left-1/2 transform -translate-x-1/2 md:left-[10px] md:transform-none bottom-[50px] z-0">
-        <div className="w-[450px] h-[450px] bg-[#FF4D2A] opacity-30 blur-3xl rounded-full"></div>
+      <div className="absolute left-0 bottom-[50px] z-0 md:left-[10px]">
+        <div className="w-[300px] h-[300px] md:w-[450px] md:h-[450px] bg-[#FF4D2A] opacity-30 blur-3xl rounded-full"></div>
       </div>
 
       <div className="w-full flex justify-center z-0 mt-[-100px]">
