cd378c6136ab1a0f1f219a2659476ba498331b94
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-04-07 04:43:19 +0100

    Update ComingUpNext.tsx

--- components/ComingUpNext.tsx
+++ components/ComingUpNext.tsx
@@ -1,7 +1,5 @@
-
 import Image from "next/image";
 import { FiArrowRight } from "react-icons/fi";
-
 
 interface Event {
   id: number;
@@ -60,14 +58,14 @@ const events: Event[] = [
 interface ComingUpNextProps {
   containerClass?: string;
 }
+
 const ComingUpNext: React.FC<ComingUpNextProps> = ({ containerClass }) => {
   function capitalize(text: string) {
-    // Ensure window is defined before accessing it
     if (typeof window !== "undefined") {
       const isMobile = window.innerWidth < 768;
       return isMobile ? text.toUpperCase() : text;
     }
-    return text; // Return original text on server-side render
+    return text;
   }
 
   return (
@@ -75,11 +73,12 @@ const ComingUpNext: React.FC<ComingUpNextProps> = ({ containerClass }) => {
       <div className="relative z-10 container mx-auto text-white">
         <h2 className="text-2xl font-semibold mb-6">Coming Up Next</h2>
 
-        <div className=" hidden md:grid grid-cols-1 lg:grid-cols-1 gap-4">
+        <div className="hidden md:grid grid-cols-1 lg:grid-cols-1 gap-4">
           {events.map((event) => (
             <div
               key={event.id}
-              className="grid flex-wrap lg:grid-cols-2 xl:grid-cols-[0.9fr_0.5fr_1fr] gap-4 items-center p-4 border border-gray-800 rounded-[5px] bg-transparent">
+              className="grid flex-wrap lg:grid-cols-2 xl:grid-cols-[0.9fr_0.5fr_1fr] gap-4 items-center p-4 border border-gray-800 rounded-[5px] bg-transparent"
+            >
               {/* Left */}
               <div className="flex gap-3 w-fit">
                 <div className="relative w-24 h-24 rounded-[5px] overflow-hidden flex-shrink-0">
@@ -111,14 +110,17 @@ const ComingUpNext: React.FC<ComingUpNextProps> = ({ containerClass }) => {
               <div className="flex justify-center gap-4 md:grid grid-cols-2 max-sm:flex-col sm:flex-col">
                 <a
                   href={event.readMoreLink}
-                  className="lg:w-fit border border-gray-700 text-gray-400 hover:text-white rounded-[5px] text-xs md:text-sm py-4 px-8 flex justify-center">
-                  Read more <FiArrowRight className="h-4 w-4 ml-1 mt-[0.2em]" />
+                  className="lg:w-fit border border-gray-700 text-gray-400 hover:text-white rounded-[5px] text-xs md:text-sm md:py-3 md:px-6 py-2 px-4 flex justify-center"
+                >
+                  Read more{" "}
+                  <FiArrowRight className="h-4 w-4 ml-1 mt-[0.2em]" />
                 </a>
 
                 <div>
                   <a
                     href={event.ticketLink}
-                    className="bg-[#ff4d2a] lg:w-fit hover:bg-[#ff4d2a]/90 text-white font-semibold rounded-[5px] text-xs md:text-sm py-4 px-8 flex justify-center lg:mr-0 lg:ml-auto">
+                    className="bg-[#ff4d2a] lg:w-fit hover:bg-[#ff4d2a]/90 text-white font-semibold rounded-[5px] text-xs md:text-sm md:py-3 md:px-6 py-2 px-4 flex justify-center lg:mr-0 lg:ml-auto"
+                  >
                     GET TICKET{" "}
                     <FiArrowRight className="h-4 w-4 ml-1 transform rotate-45" />
                   </a>
@@ -128,11 +130,12 @@ const ComingUpNext: React.FC<ComingUpNextProps> = ({ containerClass }) => {
           ))}
         </div>
 
-        <div className="md:hide-srollbar-container md:hidden flex gap-4 overflow-x-auto snap-x snap-mandatory scrollbar-hide max-md:py-4 max-md:h-[max-content]">
+        <div className="md:hidden flex gap-4 overflow-x-auto snap-x snap-mandatory scrollbar-hide max-md:py-4 max-md:h-[max-content]">
           {events.map((event) => (
             <div
               key={event.id}
-              className="snap-start max-md:bg-[#77777717] flex-shrink-0 w-[80%] max-md:w-[20em] border border-gray-800 rounded-[5px] bg-transparent p-4 max-md:py-7 max-md:px-6 max-md:grid max-md:h-[20em]">
+              className="snap-start max-md:bg-[#77777717] flex-shrink-0 w-[80%] max-md:w-[20em] border border-gray-800 rounded-[5px] bg-transparent p-4 max-md:py-7 max-md:px-6 max-md:grid max-md:h-[20em]"
+            >
               {/* Left */}
               <div className="flex gap-3 w-fit mb-2 max-sm:grid grid-cols-2 max-md:pt-[1.2em]">
                 <div className="relative w-24 h-24 rounded-[5px] overflow-hidden flex-shrink-0 max-sm:w-full max-sm:h-[8em]">
@@ -147,7 +150,7 @@ const ComingUpNext: React.FC<ComingUpNextProps> = ({ containerClass }) => {
                   <h3 className="text-[#ff4d2a] font-semibold text-sm md:text-base lg:text-lg max-md:leading-7 leading-2 max-sm:text-[1.3em]">
                     {capitalize(event.title)}
                   </h3>
-                  <p className="text-gray-400 max-md:text-white text-[0.95em]  md:text-sm">
+                  <p className="text-gray-400 max-md:text-white text-[0.95em] md:text-sm">
                     {event.location}
                   </p>
                   <p className="text-gray-400 text-xs md:text-sm">
@@ -156,22 +159,25 @@ const ComingUpNext: React.FC<ComingUpNextProps> = ({ containerClass }) => {
                 </div>
               </div>
 
-              {/* Middle*/}
+              {/* Middle */}
               <div className="mb-2 max-sm:hidden">
                 <p className="text-white text-sm">{event.date}</p>
               </div>
 
-              {/* Right*/}
+              {/* Right */}
               <div className="flex flex-col-reverse gap-3 w-[80%] mx-auto">
                 <a
                   href={event.readMoreLink}
-                  className="border border-gray-700 text-gray-400 hover:text-white rounded-[5px] text-xs md:text-sm py-2 px-4 flex justify-center">
-                  Read more <FiArrowRight className="h-4 w-4 ml-1" />
+                  className="border border-gray-700 text-gray-400 hover:text-white rounded-[5px] text-xs md:text-sm py-2 px-4 flex justify-center"
+                >
+                  Read more{" "}
+                  <FiArrowRight className="h-4 w-4 ml-1" />
                 </a>
 
                 <a
                   href={event.ticketLink}
-                  className="bg-[#ff4d2a] hover:bg-[#ff4d2a]/90 text-white font-semibold rounded-[5px] text-xs md:text-sm py-2 px-4 flex justify-center">
+                  className="bg-[#ff4d2a] hover:bg-[#ff4d2a]/90 text-white font-semibold rounded-[5px] text-xs md:text-sm py-2 px-4 flex justify-center"
+                >
                   GET TICKET{" "}
                   <FiArrowRight className="h-4 w-4 ml-1 transform rotate-45" />
                 </a>
