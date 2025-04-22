ed58a5328bb1d593681f35699e3b3993aab9f78e
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-04-22 19:34:50 +0100

    Update StatsCard.tsx

--- components/eventstats/StatsCard.tsx
+++ components/eventstats/StatsCard.tsx
@@ -16,9 +16,8 @@ const StatsCard: React.FC<StatsCardProps> = ({ label, value }) => (
       text-center
       min-h-[200px] /* ensure a minimum height */
     ">
-    <span className="text-gray-300 text-xl">{label}</span> {/* larger label */}
+    <span className="text-gray-300 text-xl">{label}</span> 
     <span className="text-6xl font-bold mt-6">{value}</span>{" "}
-    {/* bigger number */}
   </div>
 );
 
