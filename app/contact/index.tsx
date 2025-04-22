3486dc262f9699f9335ea94852ba483494dc53fd
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-04-22 19:08:00 +0100

    Update index.tsx

--- app/contact/index.tsx
+++ app/contact/index.tsx
@@ -8,19 +8,17 @@ import {
   FiLinkedin,
   FiPhoneCall,
   FiMapPin,
+  FiSend,
 } from "react-icons/fi";
 
 const ContactPage = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [date, setDate] = useState("");
-  const [topic, setTopic] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log({ firstName, lastName, phone, date, topic, message });
+    console.log({ name, email, message });
   };
 
   return (
@@ -34,21 +32,36 @@ const ContactPage = () => {
         priority
       />
 
-      <div className="relative z-10 py-10">
-        <section>
-          <h1 className="text-5xl font-bold mb-2">Contact Our Team</h1>
-          <p className="text-lg text-gray-200">
-            Got any questions about our product or related issues? We’re here to
-            help. Chat to our friendly team 24/7 and get onboard in less than 5
-            minutes.
-          </p>
+      <div className="relative z-10 pt-4 pb-10">
+        <section className="flex flex-col md:flex-row items-center justify-between gap-6 mt-4">
+          <div className="flex-1">
+            <h1 className="text-5xl font-bold mb-2">Contact our team</h1>
+            <p className="text-base text-gray-200 max-w-xl">
+              Got any questions about our product or related issues? We’re here
+              to help. Chat to our friendly team 24/7 and get onboard in less
+              than 5 minutes.
+            </p>
+          </div>
+
+          <div className="flex-none transform -translate-x-4">
+            <Image
+              src="/3dicons.png"
+              alt="Arrow Icon"
+              width={300}
+              height={300}
+              className="mx-auto"
+              priority
+            />
+          </div>
         </section>
 
         <section className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="p-6 bg-[#111111]/90 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-2">Chat with us</h2>
-            <p className="text-gray-200 mb-4">
-              Speak to our friendly team via live chat.
+          <div className="p-6 bg-[#111111]/90 rounded-[20px] shadow-lg">
+            <h2 className="text-[30px] font-semibold mb-2 text-[#F68B61]">
+              Chat with us
+            </h2>
+            <p className="text-[11px] text-gray-200 mb-4">
+              Start a live chat, or send an email—whatever works best for you.
             </p>
             <ul className="space-y-2">
               <li className="flex items-center gap-2">
@@ -64,43 +77,53 @@ const ContactPage = () => {
               <li className="flex items-center gap-2">
                 <FiMail className="text-primary" />
                 <a
-                  href="mailto:support@example.com"
+                  href="mailto:support@quiktis.com"
                   className="hover:underline">
-                  Shoot us an email
+                  support@quiktis.com
                 </a>
               </li>
               <li className="flex items-center gap-2">
                 <FiLinkedin className="text-primary" />
                 <a
-                  href="https://www.linkedin.com"
+                  href="https://www.linkedin.com/company/quiktisupport"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:underline">
-                  Message us on LinkedIn
+                  QuiktisSupport
                 </a>
               </li>
             </ul>
           </div>
 
-          <div className="p-6 bg-[#111111]/90 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-2">Call us</h2>
-            <p className="text-gray-200 mb-4">
-              Call our team Mon-Fri from 8am to 5pm.
+          <div className="p-6 bg-[#111111]/90 rounded-[20px] shadow-lg">
+            <h2 className="text-[30px] font-semibold mb-2 text-[#F68B61]">
+              Call us
+            </h2>
+            <p className="text-[11px] text-gray-200 mb-4">
+              Our lines are open Mon-Fri from 8am to 5pm.
             </p>
             <ul className="space-y-2">
               <li className="flex items-center gap-2">
                 <FiPhoneCall className="text-primary" />
-                <a href="tel:+2341234567" className="hover:underline">
-                  +234 123 4567
+                <a href="tel:12995560908" className="hover:underline">
+                  1299 556-0908
+                </a>
+              </li>
+              <li className="flex items-center gap-2">
+                <FiPhoneCall className="text-primary" />
+                <a href="tel:12995560098" className="hover:underline">
+                  1299 556-0098
                 </a>
               </li>
             </ul>
           </div>
 
-          <div className="p-6 bg-[#111111]/90 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-2">Visit us</h2>
-            <p className="text-gray-200 mb-4">
-              Chat to our sponsor at our Lagos HQ.
+          <div className="p-6 bg-[#111111]/90 rounded-[20px] shadow-lg">
+            <h2 className="text-[30px] font-semibold mb-2 text-[#F68B61]">
+              Visit us
+            </h2>
+            <p className="text-[11px] text-gray-200 mb-4">
+              Meet our team at our Lagos HQ.
             </p>
             <ul className="space-y-2">
               <li className="flex items-center gap-2">
@@ -116,19 +139,19 @@ const ContactPage = () => {
               <li className="flex items-center gap-2">
                 <FiMail className="text-primary" />
                 <a
-                  href="mailto:support@example.com"
+                  href="mailto:support@quiktis.com"
                   className="hover:underline">
-                  Shoot us an email
+                  support@quiktis.com
                 </a>
               </li>
               <li className="flex items-center gap-2">
                 <FiLinkedin className="text-primary" />
                 <a
-                  href="https://www.linkedin.com"
+                  href="https://www.linkedin.com/company/quiktisupport"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:underline">
-                  Message us on LinkedIn
+                  QuiktisSupport
                 </a>
               </li>
             </ul>
@@ -136,90 +159,61 @@ const ContactPage = () => {
         </section>
 
         <section className="mt-10">
-          <h2 className="text-center text-4xl font-bold mb-6">MESSAGE US</h2>
-
+          <h2 className="text-left text-4xl font-bold mb-6">Message Us</h2>
           <form
             onSubmit={handleSubmit}
-            className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {/* Row 1*/}
-            <div className="flex flex-col">
-              <label className="text-primary font-semibold mb-1">
-                First Name *
-              </label>
-              <input
-                type="text"
-                placeholder="Choose name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                required
-                className="p-3 border border-gray-700 bg-transparent rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-primary font-semibold mb-1">
-                Last Name *
-              </label>
-              <input
-                type="text"
-                placeholder="Choose name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                required
-                className="p-3 border border-gray-700 bg-transparent rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-primary font-semibold mb-1">Phone</label>
-              <input
-                type="tel"
-                placeholder="Choose phone"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                className="p-3 border border-gray-700 bg-transparent rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none"
-              />
+            className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="flex flex-col gap-6">
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Input Full Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                  className="w-full p-3 border border-[#3e3a39] bg-transparent rounded-[10px] text-white focus:ring-2 focus:ring-orange-500 focus:outline-none"
+                />
+                <label className="absolute -top-2 left-3 px-1 text-[14px] text-[#FF4D2A] bg-[#111111]">
+                  Full Name
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  type="email"
+                  placeholder="Input Email Address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="w-full p-3 border border-[#3e3a39] bg-transparent rounded-[10px] text-white focus:ring-2 focus:ring-orange-500 focus:outline-none"
+                />
+                <label className="absolute -top-2 left-3 px-1 text-[14px] text-[#FF4D2A] bg-[#111111]">
+                  Email Address
+                </label>
+              </div>
             </div>
 
-            {/* Row 2*/}
-            <div className="flex flex-col md:col-span-1">
-              <label className="text-primary font-semibold mb-1">Date</label>
-              <input
-                type="date"
-                placeholder="mm/dd/yyyy"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                className="p-3 border border-gray-700 bg-transparent rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none"
-              />
-            </div>
-            <div className="flex flex-col md:col-span-2 md:row-span-2">
-              <label className="text-primary font-semibold mb-1">Message</label>
-              <textarea
-                rows={6}
-                placeholder="Leave us a message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className="p-3 border border-gray-700 bg-transparent rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none h-full"
-              />
-            </div>
-
-            {/* Row 3 */}
-            <div className="flex flex-col md:col-span-1">
-              <label className="text-primary font-semibold mb-1">Topic</label>
-              <input
-                type="text"
-                placeholder="Click to select Topic"
-                value={topic}
-                onChange={(e) => setTopic(e.target.value)}
-                className="p-3 border border-gray-700 bg-transparent rounded-md focus:ring-2 focus:ring-orange-500 focus:outline-none"
-              />
-            </div>
-
-            {/* Row 4 */}
-            <div className="flex justify-end md:col-span-3">
-              <Button
-                type="submit"
-                className="bg-primary py-3 px-6 text-white font-semibold rounded-md">
-                Send Message
-              </Button>
+            <div className="flex flex-col justify-between">
+              <div className="relative">
+                <textarea
+                  rows={5}
+                  placeholder="Leave us a message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
+                  className="w-full p-3 border border-[#3e3a39] bg-transparent rounded-[10px] text-white focus:ring-2 focus:ring-orange-500 focus:outline-none resize-none"
+                />
+                <label className="absolute -top-2 left-3 px-1 text-[14px] text-[#FF4D2A] bg-[#111111]">
+                  Message
+                </label>
+              </div>
+              <div className="flex justify-end mt-4">
+                <Button
+                  type="submit"
+                  className="flex items-center bg-primary py-3 px-6 text-white font-semibold rounded-md hover:bg-primary/90 transition-colors">
+                  <span className="mr-2">Send Message</span>
+                  <FiSend />
+                </Button>
+              </div>
             </div>
           </form>
         </section>
