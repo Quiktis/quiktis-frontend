bb77f59cdb6419e8d830ca15836a2d2fb9447330
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-05-09 04:13:10 +0100

    Update page.tsx

--- app/live-chat/page.tsx
+++ app/live-chat/page.tsx
@@ -1,4 +1,3 @@
-// app/page.tsx
 "use client";
 
 import ChatInterfaceWrapper from "@/components/livechat/chat-interface-wrapper";
@@ -6,7 +5,6 @@ import ChatInterfaceWrapper from "@/components/livechat/chat-interface-wrapper";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-start">
-      {/* Now starts flush under your navbar */}
       <ChatInterfaceWrapper />
     </main>
   );
