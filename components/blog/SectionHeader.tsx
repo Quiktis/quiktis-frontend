1cac405e8095bea46bb2f2da30ee30b81b4669b0
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-06-09 19:54:35 +0100

    Update SectionHeader.tsx

--- components/blog/SectionHeader.tsx
+++ components/blog/SectionHeader.tsx
@@ -6,20 +6,28 @@ interface SectionHeaderProps {
 }
 
 const SectionHeader: React.FC<SectionHeaderProps> = ({ title, subtitle }) => (
-  <div className="relative max-w-4xl mb-12 overflow-hidden md:overflow-visible">
+  <div className="relative w-full mb-12 overflow-hidden md:overflow-visible">
     <div
       className="
         absolute
         left-1/2 top-[-30vw]
         w-[100vw] h-[60vw]
         transform -translate-x-1/2
-        -z-10 radial-gradient-blue blur-3xl opacity-30
+        -z-10 radial-gradient-blue blur-3xl opacity-30 
 
         md:top-[-15em]
         md:w-[50em] md:h-[30em]
       "></div>
-    <h1 className="text-5xl font-bold mb-4 text-white">{title}</h1>
-    {subtitle && <p className="text-gray-300 text-lg">{subtitle}</p>}
+    
+    <h1 className="text-6xl md:text-7xl font-bold mb-4 text-white text-left">
+      {title}
+    </h1>
+
+    {subtitle && (
+      <p className="text-gray-300 text-base text-left w-full">
+        {subtitle}
+      </p>
+    )}
   </div>
 );
 
