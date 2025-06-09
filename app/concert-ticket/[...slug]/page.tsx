b6406f226ffe0ac16c03fc845d14d06e5f35d7c6
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-06-09 18:05:24 +0100

    Update page.tsx

--- app/concert-ticket/[...slug]/page.tsx
+++ app/concert-ticket/[...slug]/page.tsx
@@ -52,14 +52,17 @@ import { formatToHumanReadableDate, formatToHumanReadableTime} from "@/app/utils
   return (
     <main className="flex min-h-screen flex-col items-center justify-start pt-16 px-5">
       <ConcertTicket
-        eventName={event?.title?? ""}
-        eventCreator={event?.organiser?.name?? ""}
-        date={formatToHumanReadableDate(event?.startDate ?? "")}
-        time={formatToHumanReadableTime(event?.startTime ?? "")}
-        venue={event?.location ?? ""}
-        ticketType={event?.tickets?.[0]?.name ?? ""}
-        ticketHolder={`${user.name?? ""}`}
-        ticketNumber={`#TXN-${trxref}`}
+        eventName="AFRO VIBES CONCERT 2025"
+        eventCreator="By mention Creator"
+        date="APRIL 15, 2025"
+        time="7:00 PM"
+        venue="LAGOS CONVENTION CENTER, VICTORIA ISLAND"
+        ticketType="GENERAL ADMISSION"
+        ticketHolder="JOHN DOE"
+        ticketNumber="ETK-2049-AFRO"
+        seatNumber="387"
+        logoUrl="/authImage.png"
+        qrCodeUrl="/qrcode.png"
       />
     </main>
   );
