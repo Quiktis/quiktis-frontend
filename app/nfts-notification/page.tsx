8a41536f933e420134d7d47accfb79b40e03a3b2
DasmineNory <104540095+DasmineNory@users.noreply.github.com>
2025-06-10 13:50:18 +0100

    Update page.tsx

--- app/nfts-notification/page.tsx
+++ app/nfts-notification/page.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// app/nfts-notification/page.tsx
 "use client";
 
 import React from "react";
@@ -23,28 +21,3 @@ export default function NftsNotificationPage() {
     </div>
   );
 }
-=======
-"use client";
-
-import React from "react";
-import SearchBar from "@/components/nfts-notifications/SearchBar";
-import NotificationsSection from "@/components/nfts-notifications/NotificationsSection";
-import StatsSection from "@/components/nfts-notifications/StatsSection";
-import GallerySection from "@/components/nfts-notifications/GallerySection";
-
-export default function NftsNotificationPage() {
-  return (
-    <div className="min-h-screen text-white">
-      <div className="container mx-auto px-4 py-8">
-        <SearchBar />
-
-        <NotificationsSection />
-
-        <StatsSection />
-
-        <GallerySection />
-      </div>
-    </div>
-  );
-}
->>>>>>> 1c28eb03d8b868877cdffd66f40c2c7f3ac069fc
